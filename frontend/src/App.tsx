3e7285eef8e98ee3758330e5e2db4cf677077cdd
stevennx <steven.xu1589@gmail.com>
2023-09-23 17:21:43 -0400

    add focus nad

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,7 @@
-import React, { useEffect, useRef } from "react";
+import React, { useCallback, useEffect, useRef } from "react";
 import { ForceGraph3D } from "react-force-graph";
-import GraphData from "./data/etherscan.json";
+import GraphData from "./data/blocks.json";
+// import GraphData from "./data/etherscan.json";
 import { UnrealBloomPass } from "three/examples/jsm/postprocessing/UnrealBloomPass.js";
 import * as THREE from "three";
 
@@ -17,14 +18,39 @@ function App() {
     // @ts-ignore
     fgRef.current.postProcessingComposer().addPass(bloomPass);
   }, []);
+
+  const focusNode = useCallback(
+    (node: any) => {
+      // Aim at node from outside it
+      const distance = 40;
+      const distRatio = 1 + distance / Math.hypot(node.x, node.y, node.z);
+
+    // @ts-ignore
+      fgRef.current.cameraPosition(
+        { x: node.x * distRatio, y: node.y * distRatio, z: node.z * distRatio }, // new position
+        node, // lookAt ({ x, y, z })
+        3000 // ms transition duration
+      );
+    },
+    [fgRef]
+  );
   return (
     <div className="App">
       <ForceGraph3D
         ref={fgRef}
-        backgroundColor="#000003"
+        backgroundColor="#141414"
         graphData={GraphData}
         nodeLabel="id"
         nodeAutoColorBy="user"
+        showNavInfo={false}
+        // fix nodes positions after dragging
+        onNodeDragEnd={(node) => {
+          node.fx = node.x;
+          node.fy = node.y;
+          node.fz = node.z;
+        }}
+        // camera focus adjustment on zoom
+        onNodeClick={focusNode}
       />{" "}
     </div>
   );
