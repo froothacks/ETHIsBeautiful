425209ddadb4edcfe1cd5973185d5df2fce27188
stevennx <steven.xu1589@gmail.com>
2023-09-23 17:10:30 -0400

    add unreal bloom effect

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,31 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import { ForceGraph3D } from "react-force-graph";
-// import GraphData from "./data/blocks.json";
 import GraphData from "./data/etherscan.json";
+import { UnrealBloomPass } from "three/examples/jsm/postprocessing/UnrealBloomPass.js";
+import * as THREE from "three";
 
 function App() {
+  const fgRef = useRef();
+
+  useEffect(() => {
+    const bloomPass = new UnrealBloomPass(
+      new THREE.Vector2(window.innerWidth, window.innerHeight),
+      0.4,
+      0.5,
+      0.7
+    );
+    // @ts-ignore
+    fgRef.current.postProcessingComposer().addPass(bloomPass);
+  }, []);
   return (
     <div className="App">
       <ForceGraph3D
+        ref={fgRef}
+        backgroundColor="#000003"
         graphData={GraphData}
-        nodeLabel={(node: any) => `${node.user}: ${node.description}`}
-        nodeAutoColorBy={"user"}
-      />
+        nodeLabel="id"
+        nodeAutoColorBy="user"
+      />{" "}
     </div>
   );
 }
