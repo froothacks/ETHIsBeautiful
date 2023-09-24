5088545b6a38b816f1c4a64c8df177f601620a76
stevennx <steven.xu1589@gmail.com>
2023-09-24 17:13:44 -0400

    increase threshold

--- frontend/src/utils/constants.ts
+++ frontend/src/utils/constants.ts
@@ -2,4 +2,4 @@
 export const TIME_INTERVAL_MS = 180000; // 3 minutes in milliseconds
 export const CADENCE_MS = 250; // 1/4 of a second in milliseconds
 export const NUM_BUCKETS = TIME_INTERVAL_MS / CADENCE_MS;
-export const TXN_THRESHOLD = 20;
+export const TXN_THRESHOLD = 100;
